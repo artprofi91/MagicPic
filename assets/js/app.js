2928e1ae36cf367e8abe14efbea4114e5895ef58
Артем Трегубчак <Artprofi91@MacBook-Pro-Artem.local>
2017-08-02 20:46:16 -0500

    frebase counter here

--- assets/js/app.js
+++ assets/js/app.js
@@ -170,20 +170,22 @@ $(document).ready(function() {
 
   // when you clicl celebrity do something
   $('#celebrity').click(function () {
-    $("#food").addClass("hide");
+    $(".searching").addClass("hide");
     $(".h3-URL").removeClass("hide");
     $("#imgurl").removeClass("hide");
     $("#filename").removeClass("hide");
     $(".star").removeClass("hide");
+    $('<h2>You are searching for celebrity</h2>').appendTo('.celFood')
   });
 
    // when you clicl food do something
   $('#food').click(function () {
-    $("#celebrity").addClass("hide");
+    $(".searching").addClass("hide");
     $(".h3-URL").removeClass("hide");
     $("#imgurl").removeClass("hide");
     $("#filename").removeClass("hide");
     $(".food").removeClass("hide");
+    $('<h2>You are searching for food</h2>').appendTo('.celFood')
   });
 
   // check do you paste something or not
@@ -254,10 +256,41 @@ $(document).ready(function() {
  
 }); // end of document ready function
 
-
+// =====================firebase============================
 // firebase reference
 var database = firebase.database();
+// Setting initial value of our click counter variable to 0
+    var clickCounter = 0;
+    // FUNCTIONS + EVENTS
+    // --------------------------------------------------------------------------------
+    // On Click of Button
+    $("#celebrity, #food").on("click", function() {
+      // Add to clickCounter
+      clickCounter++;
+      //  Store Click Data to Firebase in a JSON property called clickCount
+      // Note how we are using the Firebase .set() method
+      database.ref().set({
+        clickCount: clickCounter
+      });
+    });
+    // MAIN PROCESS + INITIAL CODE
+    // --------------------------------------------------------------------------------
+    // Using .on("value", function(snapshot)) syntax will retrieve the data
+    // from the database (both initially and every time something changes)
+    // This will then store the data inside the variable "snapshot". We could rename "snapshot" to anything.
+    database.ref().on("value", function(snapshot) {
+      // Then we change the html associated with the number.
+      $("#click-value").html(snapshot.val().clickCount);
+      // Then update the clickCounter variable with data from the database.
+      clickCounter = snapshot.val().clickCount;
+      // If there is an error that Firebase runs into -- it will be stored in the "errorObject"
+      // Again we could have named errorObject anything we wanted.
+    }, function(errorObject) {
+      // In case of error this will print the error
+      console.log("The read failed: " + errorObject.code);
+    });
 
+// ===============end of firebase========================
 
 // =========================wikiAPI============================
 
